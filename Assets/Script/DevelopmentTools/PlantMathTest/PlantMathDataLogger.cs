bfe4e5fd2542c8703fe2d87633db01304e723fda
Ziyu Li <vpollolee@gmail.com>
2025-03-26 04:35:18 -0400

    CHECK: sanity check

--- Assets/Script/DevelopmentTools/PlantMathTest/PlantMathDataLogger.cs
+++ Assets/Script/DevelopmentTools/PlantMathTest/PlantMathDataLogger.cs
@@ -1,18 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using GameConstant;
 using UnityEngine;
 
 public class PlantMathDataLogger : MonoBehaviour
 {
     public enum TrackedPlantDataType
     {
-        PlantHealth,
-        PlantEnergy
+        Health,
+        Energy,
+        RootMass,
+        Height,
+        Water,
+        NutrientA,
+        NutrientB,
+        NutrientC,
+        NutrientD,
+        Age,
+    }
+
+    public enum TrackedSoilDataType
+    {
+        
     }
     
     private List<PlantBehaviorLocalTest> _trackedPlants;
     private Dictionary<PlantBehaviorLocalTest, Dictionary<TrackedPlantDataType, List<float>>> _plantDataBanks;
+    private List<GridBehaviorLocalTest> _trackedSoil;
+    private Dictionary<GridBehaviorLocalTest, Dictionary<TrackedSoilDataType, List<float>>> _soilDataBanks;
 
     public static PlantMathDataLogger Instance;
 
@@ -39,10 +56,56 @@ public class PlantMathDataLogger : MonoBehaviour
     {
         foreach (PlantBehaviorLocalTest plant in _trackedPlants)
         {
-            _plantDataBanks[plant][TrackedPlantDataType.PlantHealth].Add(plant.Health);
-            _plantDataBanks[plant][TrackedPlantDataType.PlantEnergy].Add(plant.EnergyLevel);
-            
-            Window_Graph.Instance.ShowGraph(_plantDataBanks[plant][TrackedPlantDataType.PlantEnergy], -1, i => $"tick{i}", f => $"{f}");
+            _plantDataBanks[plant][TrackedPlantDataType.Health].Add(plant.Health);
+            _plantDataBanks[plant][TrackedPlantDataType.Energy].Add(plant.EnergyLevel);
+            _plantDataBanks[plant][TrackedPlantDataType.RootMass].Add(plant.RootMass);
+            _plantDataBanks[plant][TrackedPlantDataType.Height].Add(plant.Height);
+            _plantDataBanks[plant][TrackedPlantDataType.Water].Add(plant.WaterLevel);
+            _plantDataBanks[plant][TrackedPlantDataType.NutrientA].Add(plant.NutrientLevels.nutrients.x);
+            _plantDataBanks[plant][TrackedPlantDataType.NutrientB].Add(plant.NutrientLevels.nutrients.y);
+            _plantDataBanks[plant][TrackedPlantDataType.NutrientC].Add(plant.NutrientLevels.nutrients.z);
+            _plantDataBanks[plant][TrackedPlantDataType.NutrientD].Add(plant.NutrientLevels.nutrients.w);
+            _plantDataBanks[plant][TrackedPlantDataType.Age].Add(plant.Age);
+
+            Window_Graph.Instance.ShowGraph(_plantDataBanks[plant][TrackedPlantDataType.Energy], -1, i => $"tick{i}", f => $"{f}");
         }
+    }
+
+    public void Retract()
+    {
+        foreach (PlantBehaviorLocalTest plant in _trackedPlants)
+        {
+            Dictionary<TrackedPlantDataType, List<float>> dataBank = _plantDataBanks[plant];
+            if (dataBank[TrackedPlantDataType.NutrientA].Count < 1) return;
+            plant.SetInitialProperties(new PlantInitInfo(
+                dataBank[TrackedPlantDataType.RootMass].RemoveLastReturnNewLast(),
+                dataBank[TrackedPlantDataType.Height].RemoveLastReturnNewLast(),
+                dataBank[TrackedPlantDataType.Energy].RemoveLastReturnNewLast(),
+                dataBank[TrackedPlantDataType.Health].RemoveLastReturnNewLast(),
+                dataBank[TrackedPlantDataType.Age].RemoveLastReturnNewLast(),
+                dataBank[TrackedPlantDataType.Water].RemoveLastReturnNewLast(),
+                new Vector4(
+                    dataBank[TrackedPlantDataType.NutrientA].RemoveLastReturnNewLast(), 
+                    dataBank[TrackedPlantDataType.NutrientB].RemoveLastReturnNewLast(), 
+                    dataBank[TrackedPlantDataType.NutrientC].RemoveLastReturnNewLast(), 
+                    dataBank[TrackedPlantDataType.NutrientD].RemoveLastReturnNewLast())
+            ));
+            
+            Window_Graph.Instance.ShowGraph(_plantDataBanks[plant][TrackedPlantDataType.Energy], -1, i => $"tick{i}", f => $"{f}");
+        }
+    }
+}
+
+public static class ListExtensions
+{
+    public static T RemoveLastReturnNewLast<T>(this List<T> list)
+    {
+        if (list == null || list.Count == 0)
+            throw new InvalidOperationException("The list is empty.");
+
+        int lastIndex = list.Count - 1;
+        T lastElement = list[lastIndex-1];
+        list.RemoveAt(lastIndex);
+        return lastElement;
     }
 }
